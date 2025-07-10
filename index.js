0b90f8c5a1a51b82b45c9b2516be98a325582b0a
momobln <kashshaazam@gmail.com>
2025-07-10 19:21:32 +0200

    Done JavaScript

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const newStr = s1 + " " + s2 + " " + s3 +" " + s4 + " " + s5 + " "+ s3 + " " + s2 + " " + s1 + " " + s4
 
 // Print out the concatenated string
+console.log(newStr);
 
 
 
@@ -22,11 +24,18 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const lastLetterPart1 = part1[part1.length - 1].toUpperCase();
+const lastLetterPart2 = part2[part2.length - 1].toUpperCase();
+const wordsWithOutLetter1 = part1.slice(0, -1)
+const wordsWithOutLetter2 = part2.slice(0, -1)
+const newStr2 = wordsWithOutLetter1 + lastLetterPart1 + wordsWithOutLetter2 + lastLetterPart2
 
 
 // Print the cameLtaiL-formatted string
-
-
+console.log(lastLetterPart1, lastLetterPart2)
+console.log(wordsWithOutLetter1);
+console.log(wordsWithOutLetter2);
+console.log(newStr2);
 
 
 /*******************************************
@@ -35,12 +44,15 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tip = billTotal * 0.15;
 
 
 // Print out the tipAmount
+console.log(tip);
 
-
-
+//Calculate Tip
+const tipAmount = billTotal * 0.15;
+console.log(tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,9 +60,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -74,3 +88,11 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
